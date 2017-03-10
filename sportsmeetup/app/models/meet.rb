c14bbd2a053c2a23c1ada4179efc0ef5b52b81ea
J Jordan <jjordan2@uccs.edu>
2017-03-10 01:31:20 +0000

    Added a DB schema skeleton to db/seeds.rb, added model skeletons to app/models. Not sure why meet.rb had class User in it so moved User to user.rb and reimplemented Meet in meet.rb. Kept the two extra lines from User in Meet for just-in-case reasons.

--- sportsmeetup/app/models/meet.rb
+++ sportsmeetup/app/models/meet.rb
@@ -1,5 +1,6 @@
-class User < ActiveRecord::Base
+class Meet < ActiveRecord::Base
     
+    #Preserved, no idea if "User" class was a typo so cloned that to user.rb - J
     geocoded_by :address
     after_validation :geocode
     
--- sportsmeetup/db/seeds.rb
+++ sportsmeetup/db/seeds.rb
@@ -1,23 +1,64 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Feel free to seed empty quotes with whatever you'd like
 
-movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
-    	  {:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984'},
-    	  {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'},
-      	  {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011'},
-      	  {:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001'},
-      	  {:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001'},
-      	  {:title => '2001: A Space Odyssey', :rating => 'G', :release_date => '6-Apr-1968'},
-      	  {:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004'},
-      	  {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'},
-      	  {:title => 'Chicken Run', :rating => 'G', :release_date => '21-Jun-2000'},
-  	 ]
+# id			unique key for meet
+# name			name of the meet
+# league		league associated with (nil for now but consider default league?)
+# sport			sport being played (nil for now but consider default sport?)
+# datetime		unix timestamp of time meet
+#				(potentially easiest to convert to user's current timezone)
+# location		physical location name of meet
+# lat			latitude (from/for google maps API?)
+# lng			longitude (from/for google maps API?)
+# info			additional info of meet
 
-movies.each do |movie|
-  Movie.create!(movie)
+meets = [
+	{
+		:id => 0, :name => '', :league => 0, :sport => nil, :max_sign_ups => 3
+		:datetime => 0, :location => '', :lat => 0, :lng => 0,
+		:info => ''
+	},
+	{
+		:id => 1, :name => '', :league => 0, :sport => nil, :max_sign_ups => 10
+		:datetime => 0, :location => '', :lat => 0, :lng => 0,
+		:info => ''
+	},
+	{
+		:id => 2, :name => '', :league => 0, :sport => nil, :max_sign_ups => 10
+		:datetime => 0, :location => '', :lat => 0, :lng => 0,
+		:info => ''
+	},
+	{
+		:id => 3, :name => '', :league => 0, :sport => nil, :max_sign_ups => 5
+		:datetime => 0, :location => '', :lat => 0, :lng => 0,
+		:info => ''
+	},
+]
+
+meets.each do |meet|
+	Meet.create! meet
 end
+
+# Leagues LU, has default league for now
+leagues = [
+	{ :id => 0, :name => 'Casual' }
+]
+
+leagues.each do |league|
+	League.create! league
+end
+
+
+# LU for sign-ups. id not unique, use by select id where
+sign_ups = [
+	{:id => 0, :person_first_name => '', :person_last_name => ''}
+	{:id => 0, :person_first_name => '', :person_last_name => ''}
+	{:id => 0, :person_first_name => '', :person_last_name => ''}
+	{:id => 3, :person_first_name => '', :person_last_name => ''}
+]
+
+sign_ups each do |sign_up|
+	SignUp.create! sign_up
+end
+
+# At some point, may need a logins table
+
